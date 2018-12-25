ef56524684b7564b6ad12c2a696985e5e0e921a2
naman sharma <44020825+namankrs@users.noreply.github.com>
2018-12-25 12:34:41 +0530

    introduce moveSnake()

---
+++ game.js
@@ -0,0 +1,3 @@
+let { moveSnake } = require("./src/lib");
+
+console.log(moveSnake({ x: 10, y: 10 }, 100000));
--- src/lib.js
+++ src/lib.js
@@ -1,9 +1,29 @@
 const ax = require("axel");
 
-generateBackdrop(){
+const generateBackdrop = function() {
   ax.clear();
   ax.bg(255, 255, 255);
   ax.box(1, 20, 120, 30);
-}
+};
 
+const generateSnake = function({ x, y }) {
+  ax.bg(0, 255, 0);
+  ax.box(x, y, 2, 1);
+};
 
+const generateNextPos = function({ x, y }) {
+  x = x + 1;
+  return { x, y };
+};
+
+const moveSnake = function(initCordinate, cycles) {
+  for (let move = 0; move < cycles; move++) {
+    generateBackdrop();
+    generateSnake(initCordinate);
+    initCordinate = generateNextPos(initCordinate);
+  }
+};
+
+module.exports = {
+  moveSnake
+};
